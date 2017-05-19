fb7674b3e449907c80076421dd385556f8f2c93d
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2017-05-19 12:57:06 +0530

    now it works!!

--- src/launcher.py
+++ src/launcher.py
@@ -2,10 +2,10 @@
 
 #
 #   FOX LAUNCHER
-#   
+#
 #   Created for Fur Desktop Environment
 #   http://github.com/krushndayshmookh/fur-box
-#     
+#
 #   Created by Krushn Dayshmookh
 #   http://krushndayshmookh.github.io
 #
@@ -47,104 +47,104 @@ def readAppName (desktopFile):
             prop[1] = prop[1].replace("\n","")
             prop[1] = prop[1].replace(" ","-")
             x.name = prop[1]
-          
-        if prop[0] == "TryExec":
+
+        if prop[0] == "Exec":
             prop[1] = prop[1].replace("\n","")
             x.executable = prop[1]
-           
+
         if prop[0] == "Icon":
             prop[1] = prop[1].replace("\n","")
             x.icon = prop[1]
             #print (appIcon + "\n")
-            
+
         if prop[0] == "Categories":
             prop[1] = prop[1].replace("\n","")
             categories = prop[1].split(";")
             x.category = categories
-            
+
         if prop[0] == "NoDisplay":
             prop[1] = prop[1].replace("\n","")
             if prop[1] == "true":
                 shouldAppBeShown = False
-            
+
         #app = AnApp(appName, appExec, appIcon)
-    
+
         #
     #print (x.name)
     if shouldAppBeShown:
         appList.append(x)
 
-    #print (appList[0].name,appList[0].executable,appList[0].icon,)        
+    #print (appList[0].name,appList[0].executable,appList[0].icon,)
     file.close()
 
 
-for filename in files:    
+for filename in files:
     readAppName("/usr/share/applications"+"/"+filename)
 #readAppName("deluge.desktop")
 
 
-def list():  
+def list():
     for app in appList:
         print (app.name)
 
-    
-#print (appList)    
-    
+
+#print (appList)
+
 def runProgram(program):
     for app in appList:
         if app.name == program:
             print (app.name)
             system(app.executable)
 
-            
-            
-            
-            
-            
-            
-            
-# --------------      CATEGORIES        -----------------------    
-    
+
+
+
+
+
+
+
+# --------------      CATEGORIES        -----------------------
+
 
 
 def addCategory (categoryName, program, categoryKey):
     for x in program.category:
         if x  == categoryKey:
             categoryName.append(program)
-    
-    
-    
+
+
+
 network = []
 development = []
-    
+
 for app in appList:
     addCategory(network, app, "Network")
     addCategory(network, app, "Internet")
-    addCategory(development, app, "Development")    
+    addCategory(development, app, "Development")
 
 #for app in network:
 #    print (app.name)
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
 # -------------------------------------------------------------
 
-#if __name__ == __main__:  
+#if __name__ == __main__:
 #    if sys.argv[1] == "run":
 #        runProgram(sys.argv[2])
-#    
+#
 #    if sys.argv[1] == "list":
 #        list()
-    
+
 #print sys.argv[0]
 #print sys.argv[1]
 #print sys.argv[2]
-    
-    
+
+
 # -------------------------------------------------------------------------------------------------
 #
 #       USAGE INSTRUCTIONS
@@ -153,18 +153,17 @@ for app in appList:
 #
 #
 #   after that to run "app-name" from the list use "python launcher.py run app-name"
-#     
+#
 #
 #   This is currently very much in development.
 #   It may not work at all some times.
 #
-#  
 #
 #
 #
-#     
 #
 #
 #
-#  
-
+#
+#
+#
--- src/launcherui.py
+++ src/launcherui.py
@@ -10,116 +10,64 @@ from os import system
 import launcher
 
 class LauncherWindow(Gtk.Window):
-
     def __init__(self):
-        Gtk.Window.__init__(self, title="Select Program")
+        Gtk.Window.__init__(self, title="Launch")
         self.set_default_size(600, 500)
-        
-        
+
         scrolled = Gtk.ScrolledWindow()
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        
-        #self.outerbox = Gtk.Box(spacing=6)
-        #self.add(self.outerbox)
-        
-        self.listbox = Gtk.ListBox()
-        self.listbox.set_selection_mode(Gtk.SelectionMode.NONE)
-        #self.outerbox.pack_start(self.listbox, True, True, 0)
-       
-        scrolled.add(self.listbox)
 
+        flowbox = Gtk.FlowBox()
+        flowbox.set_valign(Gtk.Align.START)
+        flowbox.set_max_children_per_line(30)
+        flowbox.set_selection_mode(Gtk.SelectionMode.NONE)
+
+        self.create_flowbox(flowbox)
+
+        scrolled.add(flowbox)
+
+        #self.add(vbox)
         self.add(scrolled)
-        
-                
+        self.show_all()
 
-        
+
+
+
+
+    def addBtn(self, name, icon, executable):
+        row = Gtk.ListBoxRow()
+        btnIcon = Gtk.IconView.new()
+
+        liststore = Gtk.ListStore(Pixbuf, str)
+        iconview = Gtk.IconView.new()
+        iconview.set_model(liststore)
+        iconview.set_pixbuf_column(0)
+        iconview.set_text_column(1)
+
+        pixbuf = Gtk.IconTheme.get_default().load_icon(icon, 48, 0)
+        liststore.append([pixbuf, name])
+
+        btn = Gtk.Button()
+        btn.connect("clicked", self.btnclicked, executable)
+        btn.add(iconview)
+        return btn
+
+
+
+    def btnclicked(self,button,executable):
+        print ("Running:", executable)
+        system(executable)
+
+    def create_flowbox (self, flowbox):
+        for app in launcher.development:
+            button = self.addBtn(app.name,app.icon,app.executable)
+            flowbox.add(button)
+        for app in launcher.network:
+            button = self.addBtn(app.name,app.icon,app.executable)
+            flowbox.add(button)
+
+
 win = LauncherWindow()
-
-btnList = []
-
-def addBtn(name,icon,executable):
-    row = Gtk.ListBoxRow()
-    btnIcon = Gtk.IconView.new()
-    
-    liststore = Gtk.ListStore(Pixbuf, str)
-    iconview = Gtk.IconView.new()
-    iconview.set_model(liststore)
-    iconview.set_pixbuf_column(0)
-    iconview.set_text_column(1)
-    
-    pixbuf = Gtk.IconTheme.get_default().load_icon(icon, 64, 0)
-    liststore.append([pixbuf, name])
-    
-    btn = Gtk.Button()
-    btn.add(iconview)
-
-	
-# IMPLEMENT THIS METHOD *ASAP*  --------------------------
-#    def btnClick (event):
-#        if event == "clicked":
-#            print ("fired!")
-#            system(executable)
-#
-#    btn.connect("clicked", btnClick)	
-# --------------------------------------------------------
-    row.add(btn)
-    btnList.append(row)
-    
-
-
-for app in launcher.network:
-    addBtn(app.name,app.icon,app.executable)
-	
-for app in launcher.development:
-    addBtn(app.name,app.icon,app.executable)
-
-for button in btnList:
-    win.listbox.add(button)
-
-
-
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
